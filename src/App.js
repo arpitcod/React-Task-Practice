2d3837f56051f038df97d1ef588f37d30a132318
Arpit Panchal <arpitpanchal2914@gmail.com>
2024-09-23 17:20:29 +0530

    adding files

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@
 import './App.css';
 
 import CounterApp2 from './ReduxTask/CounterApp/CounterApp2';
+import AuthSlice from './ReduxTask/LoginStatus/AuthSlice';
+import Login from './ReduxTask/LoginStatus/Login';
 import Theme from './ReduxTask/ThemeDarkLight/Theme';
+import MainTodoList from './ReduxTask/TodoList/MainTodoList';
+import UserName from './ReduxTask/UserName/UserName';
 
 
 function App() {
@@ -11,7 +15,7 @@ function App() {
        {/* <ProductStock quantity={0}/> */}
    
 
-   <Theme/>
+      <MainTodoList/>
         
     </div>  
   );
--- src/ReduxTask/Store.js
+++ src/ReduxTask/Store.js
@@ -2,13 +2,18 @@ import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from '../ReduxTask/BasicSetup'
 import counterAppReducer from './CounterApp/CounterSlice';
 import themeReducer from './ThemeDarkLight/ThemeSlice'
-
+import authReducer from './LoginStatus/AuthSlice'
+import userReducer from './UserName/UserNameSlice'
+import todoReducer from './TodoList/TodoListSlice'
 const store = configureStore({
 
     reducer:{
         counter : counterReducer,
         counterapp : counterAppReducer,
-        themeApp : themeReducer
+        themeApp : themeReducer,
+        authStatus: authReducer,
+        UserName: userReducer,
+        todoList : todoReducer
     }
 })
 
